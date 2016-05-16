4e67492baf2161eaf5475d5c898757c8c36c1cf6
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2016-05-16 12:38:17 -0400

    scrollto

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ var app = angular.module('webApp', ['ngSanitize'], function($interpolateProvider
   $interpolateProvider.endSymbol(']]');
 });
 
-app.controller('cumplimientoController', function($scope,$http){
+app.controller('cumplimientoController', function($scope,$http,$location,$anchorScroll){
   $scope.analisis = [];
   $scope.hash = '';
   $http.get('/data/analisis.json')
       .success(function (data) {
         $scope.analisis = data;
-        $scope.hash = window.location.hash.slice(1);
+        $scope.hash = window.location.hash.slice(2);
+        $location.hash($scope.hash);
+        $anchorScroll();
       })
       .error(function (){
         $scope.messages = { response: false, message: 'no org loaded' }
